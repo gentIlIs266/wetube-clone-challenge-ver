49cefa2521082fcd85f86380410df7ae31d92cd5
nigreos625 <160678470+gentIlIs266@users.noreply.github.com>
2024-07-20 23:12:24 +0900

    some controller function, not tested 7/20

--- src/controllers/primalController.js
+++ src/controllers/primalController.js
@@ -9,32 +9,67 @@ export const home = async (req, res) => {
     } catch (error) {
         return res.render("error", { error });
     }
-}
+};
 export const getUserJoin = (req, res) => {
     return res.render("user-template/user-join", {
         tabTitle: "Join"
     })
-}
-export const postUserJoin = (req, res) => {
+};
+export const postUserJoin = async (req, res) => {
     const {
         name, username, location,
         email, password, passwordConfirm
     } = req.body;
+    const userIsAlreadyExisting = await USER.exists({ $or: [{ username }, { email }] })
     if (password !== passwordConfirm) {
+        const formMessageWrapper = document.createElement("div");
+        const shapingThisWrapper = (element) => {
+            element.classList.add("form-message-wrapper");
+            const formMessage = document.createElement("span");
+            formMessage.textContent = "암호를 재입력하십시오."
+            element.appendChild(formMessage);
+        }
+        const passwordWarpperDiv = document.querySelector(".password-wrapper");
+        passwordWarpperDiv.classList.add("is-error");
+        passwordWarpperDiv.appendChild(shapingThisWrapper(formMessageWrapper));
         return res.status(400)
     }
+    if (userIsAlreadyExisting) {
+        return res.status(400)
+    }
+    try {
+        await USER.create({
+            name,
+            username,
+            location,
+            email,
+            passwordConfirm
+        });
+        //if user create an account, redirect to home in logged in
+        return res.redirect("/");
+    } catch (error) {
+        return res.render("error", { error });
+    }
 }
-export const getUserLogin = (req, res) => {}
-export const postUserLogin = (req, res) => {}
+export const getUserLogin = (req, res) => {
+    return res.render("user-template/user-login", {
+        tabTitle: "Log in"
+    })
+};
+export const postUserLogin = async (req, res) => {
+    const { emailOrUsername, password } = req.body;
+    //ascertain the shape of email and filter the other type(ex. num)
+    const userIsExist = await USER.findOne({})
+};
 
-export const game = (req, res) => {}
-export const podcast = (req, res) => {}
-export const youtubePremium = (req, res) => {}
-export const youtubeMusic = (req, res) => {}
-export const youtubeKids = (req, res) => {}
+export const game = (req, res) => {};
+export const podcast = (req, res) => {};
+export const youtubePremium = (req, res) => {};
+export const youtubeMusic = (req, res) => {};
+export const youtubeKids = (req, res) => {};
 
-export const watchLater = (req, res) => {}
-export const likeVideo = (req, res) => {}
+export const watchLater = (req, res) => {};
+export const likeVideo = (req, res) => {};
 
 export const watchVideo = (req, res) => {
     const {
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import mongoose, { Error } from "mongoose";
 
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
