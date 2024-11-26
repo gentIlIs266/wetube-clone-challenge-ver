951e2d9f87dcad0cde190c36247c76c0efebf529
nigreos625 <160678470+gentIlIs266@users.noreply.github.com>
2024-11-26 23:17:36 +0900

    video mixin styling done, next step is JS

--- src/client/js/home.js
+++ src/client/js/home.js
@@ -31,17 +31,5 @@ document.addEventListener("DOMContentLoaded", () => {
 
 /*
 
-'use strict'
-const timeInArray = videoInfo.meta.videoLength.split(":");
-const timeDigit = timeInArray.length;
-let ariaLabelDuration = "";
-if (timeDigit === 2) ariaLabelDuration = `${timeInArray[0]}분 ${timeInArray[1]}초`
-else if (timeDigit === 3) ariaLabelDuration = `${timeInArray[0]}시간 ${timeInArray[1]}분 ${timeInArray[2]}초`
-
-const n=new Date(),ud=videoInfo.meta.createdAt.split("_")[0],ut=videoInfo.meta.createdAt.split("_")[1],cm=n.getTime(),um=new Date(`${ud}T${ut}`).getTime(),ct=()=>{const dis=Math.floor((cm-um)/1e3);if(dis<0)return "FUTURE_VIDEO_ERROR";const yd=Math.floor(dis/31536000),mod=Math.floor(dis/2629800),wd=Math.floor(dis/604800),dd=Math.floor(dis/86400),hd=Math.floor(dis/3600),md=Math.floor(dis/60);return yd>0?`${yd}년 전`:mod>0?`${mod}달 전`:wd>0?`${wd}주 전`:dd>0?`${dd}일 전`:hd>0?`${hd}시간 전`:md>0?`${md}분 전`:`${dis}초 전`;},ago=ct();
-
-each videoInfo in DBVIDEO
-        +videoItSelf(videoInfo)
-    else
-        span NO_VIDEO_EXIST
 */
+
